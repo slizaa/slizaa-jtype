4710274e0e8261e0460bfe6f9e6a963d7e2c859e
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-04-30 08:13:23 +0200

    ~ replaced tycho-based eclipse build with "org.reficio.p2-maven-plugin"

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,7 +5,7 @@ target_dir=slizaa-scanner-jtype
 
 echo TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR
 
-PRODUCTS_DIR=$TRAVIS_BUILD_DIR/slizaa-scanner-jtype-eclipse/org.slizaa.scanner.jtype.eclipse.repository/target/repository
+PRODUCTS_DIR=$TRAVIS_BUILD_DIR/org.slizaa.scanner.jtype.eclipse.repo/target/repository
 echo PRODUCTS_DIR: $PRODUCTS_DIR
 
 filesToUpload=$(find $PRODUCTS_DIR -name "*.jar" -printf '%P\n')
