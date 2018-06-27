c2b6162dadd21d9be5832ae63308b5c935d8d0e3
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-06-28 00:25:39 +0200

    ~ first draft for hierarchical model tests

--- org.slizaa.jtype.hierarchicalgraph/src/test/java/org/slizaa/jtype/hierarchicalgraph/JTypeMapperTest.java
+++ org.slizaa.jtype.hierarchicalgraph/src/test/java/org/slizaa/jtype/hierarchicalgraph/JTypeMapperTest.java
@@ -3,21 +3,26 @@
  */
 package org.slizaa.jtype.hierarchicalgraph;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.slizaa.scanner.core.testfwk.ContentDefinitionProviderFactory.multipleBinaryMvnArtifacts;
+
+import java.util.List;
 
 import org.eclipse.emf.ecore.EFactory;
 import org.eclipse.emf.ecore.EPackage;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.slizaa.core.boltclient.testfwk.BoltClientConnectionRule;
+import org.slizaa.hierarchicalgraph.HGNode;
 import org.slizaa.hierarchicalgraph.HGRootNode;
 import org.slizaa.hierarchicalgraph.HierarchicalgraphPackage;
+import org.slizaa.hierarchicalgraph.core.algorithms.AdjacencyMatrix;
 import org.slizaa.hierarchicalgraph.graphdb.mapping.service.internal.DefaultMappingService;
 import org.slizaa.hierarchicalgraph.graphdb.model.GraphDbHierarchicalgraphPackage;
+import org.slizaa.hierarchicalgraph.graphdb.model.GraphDbNodeSource;
 import org.slizaa.hierarchicalgraph.graphdb.model.impl.CustomGraphDbHierarchicalgraphFactoryImpl;
 import org.slizaa.hierarchicalgraph.impl.CustomHierarchicalgraphFactoryImpl;
-import org.slizaa.scanner.core.testfwk.ConsoleLogProgressMonitor;
 
 /**
  * <p>
@@ -29,15 +34,14 @@ import org.slizaa.scanner.core.testfwk.ConsoleLogProgressMonitor;
 public class JTypeMapperTest {
 
   @ClassRule
-  public static JTypeSlizaaTestServerRule _server = new JTypeSlizaaTestServerRule(
-      multipleBinaryMvnArtifacts(new String[] { "org.mapstruct", "mapstruct", "1.2.0.Final" },
-          new String[] { "org.mapstruct", "mapstruct-processor", "1.2.0.Final" }));
+  public static JTypeSlizaaTestServerRule SERVER = new JTypeSlizaaTestServerRule(
+      multipleBinaryMvnArtifacts(new String[] { "com.google.guava", "guava", "23.6.1-jre" }));
 
-  @Rule
-  public BoltClientConnectionRule         _client = new BoltClientConnectionRule();
+  @ClassRule
+  public static BoltClientConnectionRule  CLIENT = new BoltClientConnectionRule();
 
-  @Test
-  public void testIt() {
+  @BeforeClass
+  public static void classInit() {
 
     EPackage.Registry.INSTANCE.put(HierarchicalgraphPackage.eNS_URI, new EPackage.Descriptor() {
       @Override
@@ -62,12 +66,59 @@ public class JTypeMapperTest {
         return new CustomGraphDbHierarchicalgraphFactoryImpl();
       }
     });
+  }
 
-    DefaultMappingService defaultMappingService = new DefaultMappingService();
+  @Test
+  public void testMatrix() {
 
-    HGRootNode rootNode = defaultMappingService.convert(new JType_Hierarchical_MappingProvider(),
-        this._client.getBoltClient(), new ConsoleLogProgressMonitor());
+    HGRootNode rootNode = new DefaultMappingService().convert(new JType_Hierarchical_MappingProvider(),
+        CLIENT.getBoltClient(), null);
 
-    System.out.println(rootNode);
+    //
+    List<HGNode> packageNodes = CLIENT.getBoltClient()
+        .syncExecCypherQuery("MATCH (:Package {fqn: 'com/google/common'})-[:CONTAINS]->(p:Package) RETURN id(p)")
+        .list(record -> rootNode.lookupNode(record.get("id(p)").asLong()));
+
+    //
+    int[][] dependencies = AdjacencyMatrix.computeAdjacencyMatrix(packageNodes);
+
+    for (int i = 0; i < dependencies.length; i++) {
+      for (int j = 0; j < dependencies[i].length; j++) {
+        System.out.print(String.format("%6d", dependencies[i][j]));
+      }
+      System.out.println();
+    }
+  }
+
+  @Test
+  public void testIt() {
+
+    HGRootNode rootNode = new DefaultMappingService().convert(new JType_Hierarchical_MappingProvider(),
+        CLIENT.getBoltClient(), null);
+
+    //
+    List<Long> list = CLIENT.getBoltClient().syncExecCypherQuery("MATCH (t:Type) WHERE t.fqn CONTAINS '$' RETURN id(t)")
+        .list(record -> record.get("id(t)").asLong());
+
+    for (Long l : list) {
+      HGNode node = rootNode.lookupNode(l);
+      assertThat(node.getParent().getNodeSource(GraphDbNodeSource.class).get().getLabels()).contains("Type");
+    }
+  }
+
+  @Test
+  public void testIt_2() {
+
+    HGRootNode rootNode = new DefaultMappingService().convert(new JType_Hierarchical_MappingProvider(),
+        CLIENT.getBoltClient(), null);
+
+    //
+    List<Long> list = CLIENT.getBoltClient().syncExecCypherQuery("MATCH (t:Type) WHERE t.fqn CONTAINS '$' RETURN id(t)")
+        .list(record -> record.get("id(t)").asLong());
+
+    for (Long l : list) {
+      HGNode node = rootNode.lookupNode(l);
+      assertThat(node.getParent().getNodeSource(GraphDbNodeSource.class).get().getLabels()).contains("Type");
+    }
   }
 }
